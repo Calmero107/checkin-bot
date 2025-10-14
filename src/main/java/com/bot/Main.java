5deebd0144ff0382e03a4dbd949e0e15d7d93701
calmero <mdl.jals107@gmail.com>
2025-10-14 17:46:38 +0700

    feat: update dummy http

--- src/main/java/com/bot/Main.java
+++ src/main/java/com/bot/Main.java
@@ -1,9 +1,12 @@
 package com.bot;
 
+import com.sun.net.httpserver.HttpServer;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.net.InetSocketAddress;
 
 @Slf4j
 public class Main {
@@ -14,12 +17,21 @@ public class Main {
             botsApi.registerBot(bot);
             log.info("Bot đang chạy với MongoDB...");
 
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.createContext("/", exchange -> {
+                String response = "Bot is running!";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                exchange.getResponseBody().write(response.getBytes());
+                exchange.close();
+            });
+            server.start();
+            log.info("🌐 Dummy server started on port 8080");
+
             // Thêm shutdown hook để đóng MongoDB connection
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 log.info("\nĐang tắt bot...");
                 MongoDBService.getInstance().close();
             }));
-
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
